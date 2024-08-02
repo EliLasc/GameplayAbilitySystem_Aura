d4483ccd54a39d0e0a48a9ea10d94e629a55f7c3
Elijah Lascurain <elijahlascurain95@gmail.com>
2024-08-02 14:46:08 -0700

    Section 3 - part 19

---
+++ GAS_Workalong/Source/GASWorkalong/Private/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//how often server updates clients
+	NetUpdateFrequency = 100.f;
+}
