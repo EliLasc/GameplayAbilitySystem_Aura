ad201a8aee824867f140b12b3db65337603d6e63
EliLas <Elijahlascurain95@gmail.com>
2024-08-19 15:01:08 -0700

    Splitting the C++ class addition files pt 2

---
+++ Aura/Intermediate/Build/Win64/x64/Aura/DebugGame/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "C:/Elijah/GASProject/GameplayAbilitySystem_Aura/Aura/Intermediate/Build/Win64/x64/Aura/DebugGame/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
---
+++ Aura/Intermediate/Build/Win64/x64/Aura/DebugGame/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Elijah/GASProject/GameplayAbilitySystem_Aura/Aura/Intermediate/Build/Win64/x64/Aura/DebugGame/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
