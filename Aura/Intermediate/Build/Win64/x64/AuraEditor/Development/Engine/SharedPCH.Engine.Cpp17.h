1fb380cf95ef0fee5a6701e9f2691ea7e4593ebf
EliLas <Elijahlascurain95@gmail.com>
2024-08-19 15:04:29 -0700

    Splitting the C++ additional files pt 3

---
+++ Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Elijah/GASProject/GameplayAbilitySystem_Aura/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.40.33813
+#include "C:/Elijah/GASProject/GameplayAbilitySystem_Aura/Aura/Intermediate/Build/Win64/x64/AuraEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h"
